4d1d6ee0da68707591efdfad1979651c4fced2b3
Shahzaib Muradani <shahzaib_muradani@hotmail.com>
2021-06-20 20:32:22 +0500

    notify

--- routes/api/notify.js
+++ routes/api/notify.js
@@ -11,21 +11,21 @@ admin.initializeApp({
 
 router.post('/send-noti', (req, res) => {
   console.log(req.body);
-  const message = {
-    notification: {
-      title: 'New Post',
-      body: 'Check event Post',
-    },
-    token:
-      'cks6-lBeQJW5po2pUY_sY1:APA91bGMtpSOVq-Q9ClPORQipnMsEHbeOJzMa7RpO2W846ldOQWSpxiuyKy3uvKc1yrYd5G5khWxyqCR3V6FWGR0cAcANVoQCrShHsToUQ3Tz2SskunQ41LuR8q2yhFUCNRvdpUecFC1',
-  };
-  admin
-    .messaging()
-    .send(message)
-    .then((res) => {
-      console.log('success', res);
-    })
-    .catch((err) => {
-      err.message;
-    });
+  // const message = {
+  //   notification: {
+  //     title: 'New Post',
+  //     body: 'Check event Post',
+  //   },
+  //   token:
+  //     'cks6-lBeQJW5po2pUY_sY1:APA91bGMtpSOVq-Q9ClPORQipnMsEHbeOJzMa7RpO2W846ldOQWSpxiuyKy3uvKc1yrYd5G5khWxyqCR3V6FWGR0cAcANVoQCrShHsToUQ3Tz2SskunQ41LuR8q2yhFUCNRvdpUecFC1',
+  // };
+  // admin
+  //   .messaging()
+  //   .send(message)
+  //   .then((res) => {
+  //     console.log('success', res);
+  //   })
+  //   .catch((err) => {
+  //     err.message;
+  //   });
 });
