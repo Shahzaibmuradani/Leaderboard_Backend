d283bb0e92ed0a7a5e0ee23c1a7f40a5f5eacabf
Shahzaib Muradani <shahzaib_muradani@hotmail.com>
2021-06-20 17:49:41 +0500

    notify

--- routes/api/notify.js
+++ routes/api/notify.js
@@ -1,4 +1,7 @@
 const admin = require('firebase-admin');
+const express = require('express');
+//var mongoose = require('mongoose');
+const router = express.Router();
 
 var serviceAccount = require('../../hearmeout-fea0b-firebase-adminsdk-tjvox-a5158c9583.json');
 
@@ -6,20 +9,23 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-const message = {
-  notification: {
-    title: 'New Post',
-    body: 'Check event Post',
-  },
-  token:
-    'cks6-lBeQJW5po2pUY_sY1:APA91bGMtpSOVq-Q9ClPORQipnMsEHbeOJzMa7RpO2W846ldOQWSpxiuyKy3uvKc1yrYd5G5khWxyqCR3V6FWGR0cAcANVoQCrShHsToUQ3Tz2SskunQ41LuR8q2yhFUCNRvdpUecFC1',
-};
-admin
-  .messaging()
-  .send(message)
-  .then((res) => {
-    console.log('success', res);
-  })
-  .catch((err) => {
-    err.message;
-  });
+router.post('/send-noti', (req, res) => {
+  console.log(req.body);
+  const message = {
+    notification: {
+      title: 'New Post',
+      body: 'Check event Post',
+    },
+    token:
+      'cks6-lBeQJW5po2pUY_sY1:APA91bGMtpSOVq-Q9ClPORQipnMsEHbeOJzMa7RpO2W846ldOQWSpxiuyKy3uvKc1yrYd5G5khWxyqCR3V6FWGR0cAcANVoQCrShHsToUQ3Tz2SskunQ41LuR8q2yhFUCNRvdpUecFC1',
+  };
+  admin
+    .messaging()
+    .send(message)
+    .then((res) => {
+      console.log('success', res);
+    })
+    .catch((err) => {
+      err.message;
+    });
+});
