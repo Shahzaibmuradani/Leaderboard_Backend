e4592a9e095b1045fe252b8a87462c658976d59a
Shahzaib Muradani <shahzaib_muradani@hotmail.com>
2021-06-21 01:16:54 +0500

    changes

--- routes/api/notify.js
+++ routes/api/notify.js
@@ -10,25 +10,23 @@ admin.initializeApp({
 });
 
 router.post('/send', async (req, res) => {
-  console.log('Hello');
   //   console.log(req.body);
-  // const message = {
-  //   notification: {
-  //     title: 'New Post',
-  //     body: 'Check event Post',
-  //   },
-  //   token:
-  //     'cks6-lBeQJW5po2pUY_sY1:APA91bGMtpSOVq-Q9ClPORQipnMsEHbeOJzMa7RpO2W846ldOQWSpxiuyKy3uvKc1yrYd5G5khWxyqCR3V6FWGR0cAcANVoQCrShHsToUQ3Tz2SskunQ41LuR8q2yhFUCNRvdpUecFC1',
-  // };
-  // admin
-  //   .messaging()
-  //   .send(message)
-  //   .then((res) => {
-  //     console.log('success', res);
-  //   })
-  //   .catch((err) => {
-  //     err.message;
-  //   });
+  const message = {
+    notification: {
+      title: 'New Post',
+      body: 'Check event Post',
+    },
+    token: req.body.token,
+  };
+  admin
+    .messaging()
+    .send(message)
+    .then((res) => {
+      console.log('success', res);
+    })
+    .catch((err) => {
+      err.message;
+    });
 });
 
 module.exports = router;
